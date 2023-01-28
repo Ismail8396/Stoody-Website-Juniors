db32f292f77e8da2f245152f10d6f22caabab828
OrkhanGG <orxan.eliyev.orxan@gmail.com>
2023-01-28 15:59:26 +0400

    SFix

--- src/main/java/com/loam/stoody/service/communication/VideoService.java
+++ src/main/java/com/loam/stoody/service/communication/VideoService.java
@@ -10,28 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 @UnderDevelopment
 @Service
 public class VideoService {
-    private final VideoRepository videoRepository;
-
-    @Autowired
-    public VideoService(VideoRepository videoRepository){
-        this.videoRepository = videoRepository;
-    }
-
-    public void uploadVideo(MultipartFile file){
-        //Video video = new Video();
-        //video.setVideoUrl(videoURL);
-
-        //videoRepository.save(video);
-    }
-
-    public void createTestVideo(){
-        Video video = new Video();
-        video.setTitle("Hello World");
-        video.setDescription("Going to");
-        video.setSubtitlesURL("receroty");
-        video.setVideoUrl("asdasd");
-        video.setThumbnailUrl("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png");
-        videoRepository.save(video);
-    }
+    //private final VideoRepository videoRepository;
 
 }
